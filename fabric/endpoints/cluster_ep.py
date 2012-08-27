a7938b8b60ce4ea65775ba66345423b1160c9586
Ashish Gaonker <ashish.gaonker@ignitesol.com>
2012-08-27 17:48:27 +0530

    moved to relational version for clustering datastore

--- fabric/endpoints/cluster_ep.py
+++ fabric/endpoints/cluster_ep.py
@@ -1,12 +1,11 @@
 from bottle import redirect
 from fabric.endpoints.http_ep import BasePlugin
-from fabric.servers.helpers.clusterenum import ClusterDictKeyEnum
 from functools import wraps
 import bottle
 class ClusteredPlugin(BasePlugin):
     '''
-    A ClusteredPlugin is a  plugin to execute check if reuest is handled by this server 
-    If not find out the correct server abd redirects to that server
+    A ClusteredPlugin is a  plugin to execute check if request is handled by this server 
+    If not find out the correct server and redirects to that server
     '''
   
     def __init__(self, default_handler=None):
@@ -23,43 +22,31 @@ class ClusteredPlugin(BasePlugin):
             
     def apply(self, callback, context):
         '''
-            # Check if this request is handled by me is_local()
-            # If not find the right server if already handled by somebody
-            #        If handled : Redirect to corresponding server
-            #        else : Find a new/existing (depending on adapter type)
+        Create the sticky value for the server.
+        Gets the server handling this sticky value
+        If there is no such server , gets the server of that type with least load %
+        Redirects to destination server if current server is NOT destination server .
+        If current server is destination server DO NOTHING . Request will be handled by this server
         '''
         server = self.get_callback_obj(callback).server
         @wraps(callback)
         def wrapper(*args, **kargs): # assuming bottle always calls with keyword args (even if no default)
-            serverdata = None
+            server_adress = None
             exception = None
             try:
-                try:
-                    channel = kargs[server.stickykey]
-                    #TODO : check first if server is of the required type ( adapter:mpeg OR adapter:CODF etc)
-                    if not server.is_local(channel):
-                        serverdata = server.get_by_stickykey(channel)
-                except KeyError:
-                    pass
-                if serverdata:
-                    print serverdata[ClusterDictKeyEnum.SERVER]
-                    if serverdata[ClusterDictKeyEnum.SERVER] != server.my_end_point:
-                        urlpath = bottle.request.environ["PATH_INFO"] + "?" + bottle.request.environ["QUERY_STRING"]
-                        print "Redirecting to : ", serverdata[ClusterDictKeyEnum.SERVER] + urlpath
-                        redirect("http://" + serverdata[ClusterDictKeyEnum.SERVER] + urlpath, 301)
-                else:
-                    #find by key if key is given (if this is re-usable adapter)
-                    try:
-                        key = kargs['key']
-                    except KeyError:
-                        key = None
-                    new_server_for_client = server.get_existing_or_free(key, server.servertype)
-                    #If current server, do not redirect
-                    print "DataStore Endpoint : ", new_server_for_client[ClusterDictKeyEnum.SERVER]
-                    print "Server endpoint ",  server.my_end_point
-#                    if new_server_for_client[ClusterDictKeyEnum.SERVER] != server.my_end_point:
-#                        redirect(new_server_for_client[ClusterDictKeyEnum.SERVER], 303)
-                
+                stickyvalue = server.create_sticky_value(kargs)
+                #TODO : check first if server is of the required type ( adapter:mpeg OR adapter:CODF etc)
+                server_adress = server.get_by_stickyvalue(stickyvalue)
+                if not server_adress:
+                    server_adress = server.get_least_loaded(server.servertype)
+                    # Update new load 
+                    # TODO find how load gets updated 
+                    # Also need to determine how load is decremented
+                    server.update_data(server_adress, load=10, stickyvalue=stickyvalue)
+                if server_adress != server.server_adress: 
+                    urlpath = bottle.request.environ["PATH_INFO"] + "?" + bottle.request.environ["QUERY_STRING"]
+                    print "Redirecting to : ", server_adress + urlpath
+                    redirect("http://" + server_adress + urlpath, 301)
                 result = callback(*args, **kargs)
             except Exception as e:
                 exception = e
@@ -68,7 +55,5 @@ class ClusteredPlugin(BasePlugin):
                 if exception:
                     raise
             return result
-        
         self.plugin_post_apply(callback, wrapper)
-        return wrapper       
-
+        return wrapper
--- fabric/servers/clusteredserver.py
+++ fabric/servers/clusteredserver.py
@@ -8,11 +8,9 @@ Also each server has access to common datastore
 from __future__ import division
 import os
 from fabric import concurrency
-from fabric.datastore.datamodule import RedisBinding
+from fabric.datastore.datamodule import MySQLBinding
 from fabric.endpoints.http_ep import methodroute, HTTPServerEndPoint
 from fabric.endpoints.cluster_ep import ClusteredPlugin
-from fabric.servers.helpers import clusterenum
-from fabric.servers.helpers.clusterenum import ClusterDictKeyEnum
 from fabric.servers.managedserver import ManagedServer
 import logging
 
@@ -23,6 +21,8 @@ elif concurrency == 'threading':
     from threading import RLock
     
 logging.getLogger().addHandler(logging.NullHandler())
+
+STICKY_VALUE_SEP = ":"
 
 class ClusteredEP(HTTPServerEndPoint):
     '''
@@ -35,25 +35,17 @@ class ClusteredEP(HTTPServerEndPoint):
         
     
     @methodroute()
-    def stop(self):
-        '''
-        This method route will stop this server
-        '''
-        pass
-    
-    
-    @methodroute()
     def status(self, channel=None):
-        my_end_point = self.server.my_end_point
+        server_adress = self.server.server_adress
         print "we are at the status process id " + str(os.getpid())
-        return "we are at the status process id " + str(os.getpid())  + " My end point is  : " + my_end_point + "\n" + \
-                "My load is : " + str(self.server.get_data(my_end_point))
+        return "we are at the status process id " + str(os.getpid())  + " My end point is  : " + server_adress + "\n" + \
+                "My load is : " + str(self.server.get_data(server_adress))
     
     @methodroute()
     def test(self):
         print os.getpid()
         return os.getpid()
-        #return self.server.redisclient.red.get(self.server.my_end_point)
+        #return self.server.redisclient.red.get(self.server.server_adress)
 
         
 class ClusterServerException(Exception):
@@ -70,19 +62,24 @@ class ClusteredServer(ManagedServer):
     ClusteredServer provides inbuilt capabilities over and above HTTPServer for clustering
     '''
 
-    def __init__(self, my_end_point, servertype, stickykey, endpoints=None, **kargs):
+    def __init__(self, server_adress, servertype, stickykeys =[], endpoints=None, **kargs):
         '''
-        :param type: defines the param type adapter MPEG, CODF etc
+        
+        :param server_adress: defines the the endpoint for the server, this is unique per server
+        :param servertype: this defines the server type 
+        :param stickykeys: It contains the list of sticky keys. The stikcy key is used to identify the stcikiness or the affinity for particular
+                           server when the request comes based on the sticky keys. The sticky value is stored by colon separated in persistent datastore
+        :param endpoints: Endpoint definition provided by cluster server by itself
         '''
-        self.datastore = RedisBinding()
+        self.datastore = MySQLBinding() # TODO : write a factory method to get the binding
         self.servertype = servertype
-        self.my_end_point = my_end_point
-        self.stickykey = stickykey or self.get_sticky_key()
+        self.server_adress = server_adress
+        self.stickykeys = stickykeys or self.get_sticky_keys()
         
         endpoints = endpoints or []
         endpoints = endpoints + [ ClusteredEP()]
         super(ClusteredServer, self).__init__(endpoints=endpoints, **kargs)
-        self.create_data(my_end_point, servertype) # This will create data if doesn't exist else updates if update flag is passed 
+        self.create_data(server_adress, servertype) # This will create data if doesn't exist else updates if update flag is passed 
         
     def get_standard_plugins(self, plugins):
         '''
@@ -92,74 +89,68 @@ class ClusteredServer(ManagedServer):
             par_plugins = super(ClusteredServer, self).get_standard_plugins(plugins)
         except AttributeError:
             par_plugins = []
-        print ( "Added the ClusteredPlugin to the server")
         return par_plugins + [ ClusteredPlugin() ] 
         
 
-    def create_data(self, my_end_point, servertype):
+    def create_data(self, server_adress, servertype):
         '''
-        This create DataStructure in Redis
+        This create DataStructure in Persistent data store
         '''
-        data_dict = {ClusterDictKeyEnum.SERVER : my_end_point , 
-                     ClusterDictKeyEnum.LOAD : 0 , 
-                     ClusterDictKeyEnum.CHANNELS :[]}
-        self.datastore.setdata(my_end_point, data_dict , servertype )
+        self.datastore.setdata(server_adress, servertype )
         
-    def get_data(self, my_end_point):
-        return self.datastore.getdata(my_end_point)
+    def get_data(self, server_adress):
+        return self.datastore.getdata(server_adress)
     
     
-    def update_data(self, my_end_point, load=None, channel=None):
-        # stciky key is same as the channel passed . Will not be the real-life case
-        self.datastore.update_server(my_end_point, channel, channel,  load)
-    
-    def get_existing_or_free(self, key , servertype, **kargs):
-        #TOBE OVERRIDDEN METHOD
+    def update_data(self, server_adress, load=None, stickyvalue=None):
+        self.datastore.update_server(server_adress, stickyvalue, load)
+        
+    def refreshed_load_on_update(self):
+        '''
+        This method defines how the load gets updated which each new request being served or completed
+        It returns new load percentage
+        '''
         pass
             
     def get_least_loaded(self, servertype):
-        pass
+        '''
+        This method gets the least loaded server of the given servertype 
+        :param servertype: this parameters contains the type of server
+        '''
+        server =  self.datastore.get_least_loaded(servertype)
+        return server.unique_key if server else None
         
     
-    def get_by_stickykey(self, stickykey,  **kargs):
+    def get_by_stickyvalue(self, stickyvalue):
         '''
-        This method get the server who is serving the given channel
-            [ Channel for mpeg : <adapter-type>/<host:port:channel-id]
-            [ Channel for CODF : <adapter-type>/<tune-id> ]
+        This method gets the server with the stickyvalue. The stickyvalue makes sure this request is handled
+        by the correct server. 
+        :param stickyvalue: stickyvalue which is handled by this server
         '''
-        key = self.datastore.get_server_by_stickykey (stickykey)
-        adapter = None
-        #Assumption we have only one key 
-        if key:
-            adapter = self.datastore.getdata(list(key)[0])
-        return adapter
+        if not stickyvalue:
+            return None
+        server =  self.datastore.get_server_by_stickyvalue(stickyvalue)
+        return server.unique_key if server else None
         
-    def get_by_key(self, key , **kargs):
+
+    def create_sticky_value(self, kargs):
         '''
-        This method will get the server who is handling the request based on the key
+        Creates sticky value from the parameters passed
+        This method get the dict of all the parameters passed to the server.
+        It extracts the all the sticky key values as specified by the given server and
+        concatenates by a separator to form a unique key  
+        :param kargs: is the map of the parameters those are passed to the server
         '''
-        return self.datastore.get_server_by_stickykey(key)
-        
-    def is_local(self, channel, **kwrags):
-        '''
-        This method checks if the request will be handled by this server or we need a redirect to the actual server.
-        If we need to find the 
-        '''
-        #Check in data store if this channel is handled already
-        #if its already handled we will redirect to that corresponding server
-        record = self.datastore.getdata(self.my_end_point)
-        return True if record and channel in record[ClusterDictKeyEnum.CHANNELS] else False
-        
-    
-    def get_load(self, **kargs):
-        '''
-        This method return the load % on this server
-        '''
-        record = self.datastore.getdata(self.my_end_point)
-        return record[ClusterDictKeyEnum.LOAD]
-    
-    
+        sticky_val = ''
+        for key in self.get_sticky_keys():
+            if sticky_val:
+                sticky_val += STICKY_VALUE_SEP
+            sticky_val = sticky_val + kargs[key]
+        return sticky_val
+            
     def get_sticky_keys(self):
-        return 'channel'
-        return None
-    
+        '''
+        This method returns the sticky keys.
+        Sticky keys may be list of individual keys , these are the params those are passed for the server request
+        '''
+        return self.stickykeys
