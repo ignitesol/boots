a953f0f361ea4c8ad0f03de25ec6c691869b455b
Ashish Gaonker <ashish.gaonker@ignitesol.com>
2012-11-15 12:08:54 +0530

    added locking while doing initial signleton object creation

--- fabric/common/singleton.py
+++ fabric/common/singleton.py
@@ -3,6 +3,11 @@ This implements a metaclass that implements the Singleton pattern. Taken from ht
 The Singleton applies to the subclass - so each subclass of Singleton forms a unique signleton (i.e the base class is not a shared singleton)
 @author: anand
 '''
+import threading
+import time
+
+class Atomic(object):
+    lock = threading.RLock() 
 
 class SingletonType(type):
     ''' introduces a Singleton type that is used as a metaclass in the Singleton class '''
@@ -12,16 +17,25 @@ class SingletonType(type):
 
     def __call__(cls, *args, **kw): #@NoSelf
         if cls.instance is None:
-            cls.instance = super(SingletonType, cls).__call__(*args, **kw)
+            with Atomic.lock:
+                if cls.instance is None:
+                    cls.instance = super(SingletonType, cls).__call__(*args, **kw)
         return cls.instance
 
 class Singleton(object):
     __metaclass__ = SingletonType
+   
+   
+
+    
+def run():
+    m = MyClass()
 
 if __name__ == '__main__':
     
     class MyClass(Singleton):
         def __init__(self):
+            time.sleep(1)
             print 'Object id for', self.__class__.__name__, 'is', id(self)
     
     
@@ -36,17 +50,20 @@ if __name__ == '__main__':
     class MultiSubClass(Dummy, Singleton):
         pass
     
-    x1 = MyClass()
-    x2 = MyClass()
+#    x1 = MyClass()
+#    x2 = MyClass()
+    for i in range(2):
+        t = threading.Thread(target=run)
+        t.start()
     
-    y1 = MySubClass()
-    y2 = MySubClass()
-    
-    z1 = MultiSubClass()
-    z2 = MultiSubClass()
-    
-    print 'Are all objects of MyClass the same?', id(x1) == id(x2)
-    print 'Are all objects of MySubClass the same?', id(y1) == id(y2)
-    print 'Are all objects of MultiSubClass the same?', id(z1) == id(z2)
-    print 'Are objects of MyClass same as objects of MySubClass?', id(x1) == id(y1)
+#    y1 = MySubClass()
+#    y2 = MySubClass()
+#    
+#    z1 = MultiSubClass()
+#    z2 = MultiSubClass()
+#    
+#    print 'Are all objects of MyClass the same?', id(x1) == id(x2)
+#    print 'Are all objects of MySubClass the same?', id(y1) == id(y2)
+#    print 'Are all objects of MultiSubClass the same?', id(z1) == id(z2)
+#    print 'Are objects of MyClass same as objects of MySubClass?', id(x1) == id(y1)
     
