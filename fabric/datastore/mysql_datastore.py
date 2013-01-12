bff65d501895c07d71f02a1fdc568ca67312db9d
Ashish Gaonker <ashish.gaonker@ignitesol.com>
2013-01-12 10:35:35 +0530

    loggers commented

--- fabric/datastore/mysql_datastore.py
+++ fabric/datastore/mysql_datastore.py
@@ -29,6 +29,7 @@ def dbsessionhandler(wrapped_fn):
     def wrapped(self, *args, **kwargs):
         sess = self.session
         try:
+            sess.flush()
             retval = wrapped_fn(self, sess,  *args, **kwargs)
         finally:
             sess.close()
@@ -65,7 +66,7 @@ class MySQLBinding(DBConnectionEndPoint):
         :param server_adress: address of the self/server itself, this will be unique entry 
         :param servertype: type of the server 
         '''
-        sess.flush()
+        #sess.flush()
         with self.__class__.internal_lock:
             try:
                 server = Server(servertype, server_adress, json.dumps({}), 0)
@@ -146,7 +147,7 @@ class MySQLBinding(DBConnectionEndPoint):
         :param endpoint_key: unique key of the endpoint
         :param endpoint_name: name of the endpoint
         '''
-        sess.flush()
+        
 #        logging.getLogger().debug("Servertype passed : %s", servertype)
         server = None
         existing_mapping_list = None
