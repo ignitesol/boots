a3131d88670d2d551a90fa2c389689c3a1203ce0
Iqbal Ansari <iqbal.ansari@ignitesol.com>
2013-10-03 18:03:17 +0530

    UI changes

--- boots/docroot/js/adminui.js
+++ boots/docroot/js/adminui.js
@@ -1,3 +1,5 @@
+/*global SPARX, request*/
+
 SPARX = {};
 
 String.prototype.toTitleCase = function(){
@@ -6,58 +8,94 @@ String.prototype.toTitleCase = function(){
 
 SPARX.Manage = function(){
 	var _config = {};
-	var prefix = "";
-	var handler_call_maps = {};
-	var save_call_maps = {};
+    var prefix = "";
+	var handlers = {};
+	var save = {};
 	var scripts_added = [];
 	return {
-		add_script: function(script_name){
-			if(scripts_added.some(function(x){return x == script_name}))
-				return;
-			var s = document.createElement("script");
-			s.src = '/boots/js/'+script_name;
-			document.head.appendChild(s);
+		/*
+		 * Add javascript to the DOM, optionally it accepts the
+		 * callbacks to be executed once the script is loaded
+		 */
+		add_script: function(script_name, callbacks) {
+            function excecuteCallbacks(callbacks) {
+				if(!callbacks) {
+					return;
+				}
+
+                if(callbacks instanceof Array) {
+                    callbacks.forEach(function(callback) {
+                        callback();
+                    });
+                }
+                else {
+                    callbacks();
+                }
+            }
+
+			// If the script is already added just execute the callbacks
+			// and return
+            if(scripts_added.indexOf(script_name) !== -1) {
+                excecuteCallbacks(callbacks);
+                return;
+            }
+
+			// Else create a script tag and append to the head of the document
+			var script = document.createElement("script");
+			script.src = '/boots/js/'+script_name;
+			
+			script.onload = function() {
+				excecuteCallbacks(callbacks);
+			};
+			
+			document.head.appendChild(script);
 			scripts_added.push(script_name);
 		},
-		setup : function(name){
-			SPARX.Manage.add_config_section_handler(name);
-			SPARX.Manage.add_config_section_save(name);
-		},
+
+		/*
+		 * Change the section currently displayed
+		 */
+        change_section : function(event) {
+            var section = event.srcElement.value;
+			SPARX.Manage.UI.show_page(section);
+        },
+
+		/*
+		 * Initialize the UI
+		 */		
 		initialize : function(new_prefix, list){
-			var interval = 0;
 			prefix = new_prefix;
-			for(var i in list){
-				var new_tab = list[i];
-				SPARX.Manage.setup(new_tab);
-				function delayed_create(interval, new_thing){
-					setTimeout(function(){
-						SPARX.Manage.UI.create_tab(new_thing);
-						SPARX.Manage.UI.activate_tab(new_thing);
-					},interval*i);
-				}
-				delayed_create(interval, new_tab);
-			}
-			setTimeout(function(){
-				SPARX.Manage.UI.activate_tab(list[0]);
-				}, interval*(list.length));
+
+			/* Add the listener responsible for changing section*/
+            document.getElementById("sections").onchange = SPARX.Manage.change_section;
+
+			/* For each section add corresponding option in dropdown list
+			 * Also fetch the data related to the section
+			 */
+			var i = -1, len = list.length;
+
+            for(; ++i < len;){
+                var new_section = list[i];
+
+				SPARX.Manage.add_config_section_handler(new_section);
+                SPARX.Manage.add_config_section_save(new_section);
+
+                SPARX.Manage.UI.add_section(new_section);
+				SPARX.Manage.get_config(new_section);
+            }
+
+			/* Finally display the default page*/
+			SPARX.Manage.UI.show_page(list[0]);
 		},
 		add_config_section_handler : function(name, func){
-			handler_call_maps[name] = func || SPARX.Manage.default_handler;
+            handlers[name] = func || SPARX.Manage.default_handler;
 		},
 		add_config_section_save : function(name, func){
-			save_call_maps[name] = func || SPARX.Manage.default_save;
+			save[name] = func || SPARX.Manage.default_save;
 		},
-		tab_clicked : function(event){
-			var tab_clicked = event.currentTarget;
-			if(tab_clicked.className === "active_tab")
-				return;
-			else{
-				SPARX.Manage.UI.activate_tab(tab_clicked.id.split("_")[0]);
-			}
-		},
+
 		get_config : function(name){
-			// console.log("Making XHR to "+prefix+'/admin/config/'+name);
-			request(prefix+'/admin/config/'+name, 
+			request(prefix+'/admin/config/'+name,
 				function(response) {
 					var curr_page_ele = document.getElementById(name+"_page");
 					curr_page_ele.innerHTML = response;
@@ -65,17 +103,18 @@ SPARX.Manage = function(){
 					curr_page_reset_ele.click();
 				},function() {});
 		},
+		
 		default_handler : function(name, config){
 			document.getElementById(name+"_content").innerHTML = syntaxHighlight(name, config);
 		},
+		
 		run_handler : function(name, config){
-			_config[name] = config; 
-			if(typeof(config) === "string")
-				config[page] = JSON.parse(config);
-			handler_call_maps[name](name, config);
+			_config[name] = config;
+			handlers[name](name, config);
 		},
+		
 		run_save : function(name){
-			new_config = save_call_maps[name](name);
+			new_config = save[name](name);
 //			config[page] = new_config;
 		},
 		get_prefix : function(){
@@ -102,48 +141,43 @@ SPARX.Manage = function(){
 		onerror : function(error, args){
 			window.alert(error);
 		}
-	}
+	};
 }();
 
 SPARX.Manage.UI = function(){
 	var current_active_tab = null;
 	var current_active_page = null;
-	
+
 	return{
 		initialize : function(){
-			
+
 		},
-		create_tab : function(new_tab){
-			var tabs = document.getElementById("tabs");
+		add_section : function(new_tab){
+            function get_option_element() {
+                return "<option onclick='SPARX.Manage.tab_clicked(" + new_tab + ");'>" + new_tab + "</option>";
+            }
+			var sections = document.getElementById("sections");
 			var pages = document.getElementById("pages");
-			tabs.innerHTML = tabs.innerHTML + "<div id='"+new_tab+"_tab' onclick='SPARX.Manage.tab_clicked(event);' class='inactive_tab'><p style='margin:5px auto;width:"+(6.5*new_tab.length)+"px;'>"+new_tab+"</p></div>";
+			// tabs.innerHTML = tabs.innerHTML + "<div id='"+new_tab+"_tab' onclick='SPARX.Manage.tab_clicked(event);' class='inactive_tab'><p style='margin:5px auto;width:"+(6.5*new_tab.length)+"px;'>"+new_tab+"</p></div>";
+            sections.innerHTML = sections.innerHTML + get_option_element();
 			pages.innerHTML = pages.innerHTML + "<div id='"+new_tab+"_page' class='inactive_page'>"+new_tab+" is loading...</div>";
 		},
 		get_current_active_tab : function(){
-					
+
 		},
 		get_current_active_page : function(){
 			return current_active_page;
 		},
-		activate_tab : function(name){
-			SPARX.Manage.UI.deactivate_all_tabs();
-			var tab_ele = document.getElementById(name+"_tab");
-			if(tab_ele){
-				tab_ele.className = "active_tab";
-				current_active_tab = tab_ele.id;
-			}
-			SPARX.Manage.UI.show_page(name+"_page");
-			document.title = document.title.split("|")[0] + "| " + name;
-			setTimeout(function(){SPARX.Manage.get_config(name);}, 200);
-		},
-		show_page : function(page_name){
+
+		show_page : function(page){
+			var page_name = page + "_page";
 			SPARX.Manage.UI.hide_all_pages();
 			var page_ele = document.getElementById(page_name);
 			if(page_ele){
 				page_ele.className = "active_page";
 				current_active_page = page_name;
 			}
-			
+			SPARX.Manage.get_config(page);
 		},
 		hide_all_pages : function(){
 			var pages = document.getElementById("pages");
@@ -156,23 +190,12 @@ SPARX.Manage.UI = function(){
 				}
 			}
 		},
-		deactivate_all_tabs : function(){
-			var tabs = document.getElementById("tabs");
-			var tab_list = tabs.children;
-			for(var i in tab_list){
-				try{
-					tab_list[i].className = "inactive_tab";
-				}catch(e){
-					break;
-				}
-			}
-		},
 		hide : function(event){
 			if(typeof(event) === "string"){
 				var ele = document.getElementById(event);
 				if(ele)
 					ele.style.display = "none";
-					
+
 			}
 			document.getElementById("popup").style.display = "none";
 		},
@@ -180,7 +203,7 @@ SPARX.Manage.UI = function(){
 			var to_ele = document.getElementById(to);
 			var from_ele = document.getElementById(from);
 			from_option_list = from_ele.children;
-			var new_from_ele_html = "";			
+			var new_from_ele_html = "";
 			var new_data = "";
 			for(var i in from_option_list){
 				if(from_option_list[i].tagName == "OPTION"){
@@ -233,17 +256,17 @@ function syntaxHighlight(section, json) {
             cls = 'null';
         }
         else if (/{/.test(match)){
-        	if(id)
-        		keys.push(id);
-        	return '<ol>'
+            if(id)
+                keys.push(id);
+            return '<ol>';
         }
         else if (/}/.test(match)){
-        	keys = [];
-        	return '</ol>'
+            keys = [];
+            return '</ol>';
         }
         else if (/,/.test(match)){
-        	keys = keys.slice(0, -1);
-        	return ''
+            keys = keys.slice(0, -1);
+            return '';
         }
         return '<span class="' + cls + '">' + match + '</span>';
     });
--- boots/templates/loggingui.tpl
+++ boots/templates/loggingui.tpl
@@ -1,33 +1,50 @@
 %try:
-	<label>{{error}}</label>
+<label>{{error}}</label>
 %except:
-	<img id="{{section}}_reset" src="/fabric/images/reset.png" onclick='SPARX.Manage.add_script("loggingui.js");SPARX.Manage.{{section}}.setup("{{section}}");SPARX.Manage.run_handler("{{section}}",{{config}});' style="position:absolute;top:10px;right:10px;" />
-	<div id="top_portion" style="width:100%;height:50%;">
+	<img id="{{section}}_reset" src="/boots/images/reset.png" onclick='SPARX.Manage.add_script("loggingui.js", function() {
+		   SPARX.Manage.{{section}}.setup("{{section}}");
+		   SPARX.Manage.run_handler("{{section}}",{{config}});
+		 });' style="position:absolute;top:10px;right:10px;" />
+    
+		<!-- <div id="top_portion" style="width:100%;height:50%;">
 		<div id="top_right_portion" style="width:50%;height:100%;float:right;">
-			<center><h2>Filters</h2></center>
-			<select name="Filters" id="filters" size="10" style="margin:5% 0px 10% 25%;width:50%;height:60%;">
-			</select>
+		<center><h2>Filters</h2></center>
+		<select name="Filters" id="filters" size="10" style="margin:5% 0px 10% 25%;width:50%;height:60%;">
+		</select>
 		</div>
-	</div>
+		</div> -->
+
 	<div id="bottom_portion" style="width:100%;height:50%;">
-		<div id="bottom_left_portion" style="width:50%;height:100%;float:left;">
-			<center><h2>Loggers</h2></center>
-			<select name="Loggers" id="loggers" size="10" style="margin:5% 0px 10% 25%;width:50%;height:60%;">
-			</select>
-		</div>
+		<!-- <div id="bottom_left_portion" style="width:50%;height:100%;float:left;">
+		<center><h2>Loggers</h2></center>
+		<select name="Loggers" id="loggers" size="10" style="margin:5% 0px 10% 25%;width:50%;height:60%;">
+        </select>
+		</div> -->
+		<div id="top_right_portion" style="width:50%;height:100%;float:right;">
+            <center><h2>Filters</h2></center>
+            <select name="Filters" id="filters" size="10" style="margin:5% 0px 10% 25%;width:50%;height:60%;">
+            </select>
+        </div>
+		
 		<div id="bottom_right_portion" style="width:50%;height:100%;float:right;">
 			<center><h2>Handlers</h2></center>
 			<select name="Handlers" id="handlers" size="10" style="margin:5% 0px 10% 25%;width:50%;height:60%;">
 			</select>
 		</div>
 	</div>
-	<div id="popup_page" style="position:fixed;width:50%;height:70%;top:15%;left:25%;z-index:1001;background-color: rgba(255, 255, 255, 0.5);border-radius: 10px;padding: 1%;display:none;">
+
+	<center>
+        <h2>Loggers</h2>
+        <div id="logger-accordion"></div>
+    </center>
+    
+	<div id="popup_page" style="position:fixed;width:96%;height:70%;top:15%;z-index:1001;background-color: rgba(255, 255, 255, 0.5);border-radius: 10px;padding: 1%;display:none; color: black;">
 		<div id="popup_content" style="float:left;width:50%;height:100%;background-color:white;border-radius: 8px;border-bottom-right-radius: 0px;border-top-right-radius: 0px;overflow:auto;">
 		</div>
 		<div id="filters" style="float:right;width:50%;height:100%;background-color:white;border-radius: 8px;border-bottom-left-radius: 0px;border-top-left-radius: 0px;">
 			<button id="add_filter_button" onclick="document.getElementById('add_filter_div').style.display='block';this.style.display='none';" style="margin:10px;float:right;">Add Filter</button>
 			<div id="add_filter_div" style="display:none;width:100%">
-				<button onclick="SPARX.Manage.Logging.add_filter();" style="margin:10px;float:right;">Done</button>
+				<button onclick="SPARX.Manage.Logging.add_filter();" style="margin:10px;float:right;">Save filter</button>
 				<table style="width:100%">
 					<tr>
 						<td>
